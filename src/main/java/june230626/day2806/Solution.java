cd91c836be8ff04d6ca05c3d195ccc2a4f806525
Tattarush <tattarush@gmail.com>
2026-06-28 17:48:24 +0300

    заново3

---
+++ src/main/java/june230626/day2806/Solution.java
@@ -0,0 +1,10 @@
+package june230626.day2806;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
